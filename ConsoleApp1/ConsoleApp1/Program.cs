2939fc99d88d70171bd273f40eea0c06cc717308
simone-art <simoneperiodista3@gmail.com>
2020-04-05 15:41:01 -0300

    Criado código para calcular idade

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            idade = (10 + 5) * 2;
+            Console.WriteLine(idade);
+        }
+    }
+}
