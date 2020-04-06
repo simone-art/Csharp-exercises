183f1561692c09b9a512c7426b667e36db79da26
simone-art <simoneperiodista3@gmail.com>
2020-04-06 18:16:08 -0300

    Criado código para somar as notas de um aluno

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,53 +5,88 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static void calculaMedia(int i, int qtd)
+        {
+            if (i > qtd) return;
+            Console.WriteLine($"{qtd}");
+            calculaMedia(i + 1, qtd);
+        }
 
-         static void Main (string[] args)
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\n\n");
+                Console.WriteLine("*************************************");
+                Console.WriteLine("Digite uma das opções abaixo");
+                Console.WriteLine("2) Execute o programa");
+                Console.WriteLine("3) Calcule a media de suas notas");
+                Console.WriteLine("4) Calcule a media da suas notas");
+                Console.WriteLine("10) Sair do programa");
+                Console.WriteLine("*************************************");
+                var n = Convert.ToInt16(Console.ReadLine());
+
+                var sair = false;
+                switch (n)
+                {
+                    case 1:
+                        Program.calculaMedia(0, 3);
+                        break;
+                    case 2:
+                        Program.calculaMediaRecursiva();
+                        break;
+                    case 3:
+                        Program.calculaMediaNotas();
+                        break;
+                    case 10:
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+                if (sair) break;
+            }
+        }
+
+        private static void calculaMediaRecursiva()
         {
             Console.WriteLine("Digite a quantidade de notas");
             var qtd = Convert.ToInt16(Console.ReadLine());
-            int[] notas = new int[qtd];
 
-            int i = 0;
-            var somaNotas = 0;
+            var notas = new List<int>();
+            notas = preencheNotasAluno(1, qtd, notas);
 
-            while (i<qtd)
-            {
-                Console.WriteLine("Digite a nota " +  (i + 1)  + " do aluno ");
-                notas[i] = Convert.ToInt16(Console.ReadLine());
-                somaNotas += notas[i]; 
-                i++;
-     
-            }
-
-            var media = somaNotas / notas.Length;
-            Console.WriteLine("A media do aluno é " + media);
-
-            switch (media)
-            {
-                case 5:
-                    Console.WriteLine("Você foi reprovado");
-                    break;
-                case 6:
-                    Console.WriteLine("Você foi aprovado mas precisa melhorar");
-                    break;
-                case 7:
-                    Console.WriteLine("Você foi aprovado. Parabéns!");
-                    break;
-                default:
-                    
-                    if (media < 5)
-                    {
-                        Console.WriteLine("Voçê foi reprovado");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Voçê foi aprovado com louvor");
-                        
-                    }
-                    break;
-
-            }
         }
+
+        
+        private static List<int> preencheNotasAluno(int i, int qtd, List<int> notas)
+        {
+            if (i > qtd) return notas;
+
+            Console.WriteLine($"Digite a {i}º nota do aluno");
+            var nota = Convert.ToInt16(Console.ReadLine());
+            notas.Add(nota);
+            return preencheNotasAluno(i + 1, qtd, notas);
+        }
+
+        private static void calculaMediaNotas()
+        {
+            int n1, n2, somaNotas;
+            n1 = Convert.ToInt16(Console.ReadLine());
+            n2 = Convert.ToInt16(Console.ReadLine());
+            somaNotas = n1 + n2;
+            Console.WriteLine("A soma das suas notas é  " + somaNotas);
+            
+           
+
+        }
+        
+
+ 
     }
+
 }
+
