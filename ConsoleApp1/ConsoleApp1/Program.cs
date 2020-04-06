0ac7bfd7c8365d2af0324c207acce7ca095658e7
simone-art <simoneperiodista3@gmail.com>
2020-04-06 10:31:22 -0300

    Criado código com switch case

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,12 +8,50 @@ namespace ConsoleApp1
 
          static void Main (string[] args)
         {
-            var nome = new List<string>();
-            nome.Add("Simone");
-            nome.Add("Silene");
-            nome.Add("Shirley");
+            Console.WriteLine("Digite a quantidade de notas");
+            var qtd = Convert.ToInt16(Console.ReadLine());
+            int[] notas = new int[qtd];
 
-            Console.WriteLine(nome[2]);
+            int i = 0;
+            var somaNotas = 0;
+
+            while (i<qtd)
+            {
+                Console.WriteLine("Digite a nota " +  (i + 1)  + " do aluno ");
+                notas[i] = Convert.ToInt16(Console.ReadLine());
+                somaNotas += notas[i]; 
+                i++;
+     
+            }
+
+            var media = somaNotas / notas.Length;
+            Console.WriteLine("A media do aluno é " + media);
+
+            switch (media)
+            {
+                case 5:
+                    Console.WriteLine("Você foi reprovado");
+                    break;
+                case 6:
+                    Console.WriteLine("Você foi aprovado mas precisa melhorar");
+                    break;
+                case 7:
+                    Console.WriteLine("Você foi aprovado. Parabéns!");
+                    break;
+                default:
+                    
+                    if (media < 5)
+                    {
+                        Console.WriteLine("Voçê foi reprovado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Voçê foi aprovado com louvor");
+                        
+                    }
+                    break;
+
+            }
         }
     }
 }
