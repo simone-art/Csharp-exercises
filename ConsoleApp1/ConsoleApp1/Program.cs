1ec741fd8d783794d4d72781c4747eebb8d32af8
simone-art <simoneperiodista3@gmail.com>
2020-04-06 18:29:42 -0300

    Feito código para calcular a media

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -74,11 +74,12 @@ namespace ConsoleApp1
 
         private static void calculaMediaNotas()
         {
-            int n1, n2, somaNotas;
+            int n1, n2, qtd, media;
             n1 = Convert.ToInt16(Console.ReadLine());
             n2 = Convert.ToInt16(Console.ReadLine());
-            somaNotas = n1 + n2;
-            Console.WriteLine("A soma das suas notas é  " + somaNotas);
+            qtd = Convert.ToInt16(Console.ReadLine());
+            media = (n1 + n2) / qtd;
+            Console.WriteLine("A media das suas notas é " + media);
             
            
 
